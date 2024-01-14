7873fdb5c127b07590eefe8f58832ede8b243f19
felipeCadena <felipecaden@gmail.com>
2024-01-14 19:27:31 -0300

    content created

---
+++ src/data/mock.ts
@@ -0,0 +1,26 @@
+export const mock = [
+  {
+    id: '1',
+    photo: 'https://cdn.xsd.cz/resize/4640bbe64316348292fbaf85629f46cd_resize=680,383_.jpg?hash=9a46751e63e2e02dbc9ab58e816e6122',
+    title: 'News 1',
+    description: 'About News',
+  },
+  {
+    id: '2',
+    photo: 'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+    title: 'News About Series',
+    description: 'About News',
+  },
+  {
+    id: '3',
+    photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTop61-4Ju7DW_5AWV8-_HEsVERwqrEuy0PBmYbv-Jl47J3kYcs0l8yuASurlTmsrdW9pk&usqp=CAU',
+    title: 'Secret Invasion',
+    description: 'About News',
+  },
+  {
+    id: '4',
+    photo: 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A00D62C3AE04498A6EFEA5260FAB9B6E01DC5D59991EF86C93AECA83502389CC/scale?width=1200&aspectRatio=1.78&format=jpeg',
+    title: 'The Punisher',
+    description: 'About News',
+  },
+]
